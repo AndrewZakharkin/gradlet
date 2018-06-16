044c09e611ec2e4dfd1b6730a9d94ddbf87e49b5
Andrew Zakharkin <zakharking@gmail.com>
2018-06-16 13:09:05 +0300

    Javadoc

---
+++ src/main/java/com/zakharkin/gradlet/Entry.java
@@ -0,0 +1,11 @@
+package com.zakharkin.gradlet;
+
+
+public class Entry {
+    /**
+     * @param args command line args
+     */
+    public static void main(String[]args){
+        System.out.println("Test");
+    }
+}
