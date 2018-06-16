293f936cb7f53f57ff55c8ad69bde91588c7d205
Andrew Zakharkin <zakharking@gmail.com>
2018-06-16 13:16:46 +0300

    JUnit 5.2.0

---
+++ src/test/java/com/zakharkin/gradlet/EntryTest.java
@@ -0,0 +1,18 @@
+package com.zakharkin.gradlet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntryTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
