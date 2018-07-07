15c6095a6cb48d8333535bdc765123c0744e551c
Andrew Zakharkin <zakharking@gmail.com>
2018-07-07 11:01:55 +0300

    Tried JSON

--- src/main/java/com/zakharkin/controllers/MyController.java
+++ src/main/java/com/zakharkin/controllers/MyController.java
@@ -1,16 +1,43 @@
 package com.zakharkin.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MyController {
-    @RequestMapping("qqq")
-    public @ResponseBody String cont(){
+    @RequestMapping("cont")
+    public @ResponseBody Container cont(@RequestParam(defaultValue = "qwe") String par){
         System.out.println("CONT WAS CALLED");
-        return "home ZAK";
+        return new Container("home ZAK", par);
+    }
+
+    class Container{
+        private String val1;
+        private String val2;
+
+        public Container(){
+
+        }
+
+        public Container(String v1, String v2){
+            val1 = v1;
+            val2 = v2;
+        }
+
+        public String getVal1() {
+            return val1;
+        }
+
+        public void setVal1(String val1) {
+            this.val1 = val1;
+        }
+
+        public String getVal2() {
+            return val2;
+        }
+
+        public void setVal2(String val2) {
+            this.val2 = val2;
+        }
     }
 }
